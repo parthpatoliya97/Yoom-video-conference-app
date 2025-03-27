25824ed98ef8078a9d9a51bd5330d6bf50055f4e
parth patoliya <parthpatoliya001@gmail.com>
2025-03-27 17:56:37 +0530

    add the changes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Yoom",
+  description: "Video Conferencing App Built By Clerk,Stream,Next.js,Typescript,Tailwind CSS",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+   
+    <main>
+    <StreamVideoProvider>
+    {children}
+    </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import 'react-datepicker/dist/react-datepicker.css'
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Yoom",
+  description: "Video Conferencing App Built By Clerk,Stream,Next.js,Typescript,Tailwind CSS",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+      appearance={
+        {
+          layout:{
+            logoImageUrl:'/icons/yoom-logo.svg',
+            socialButtonsVariant:'iconButton'
+          },
+          variables:{
+            colorText: '#fff',
+            colorPrimary:'#0E78F9',
+            colorBackground:'#1c1f2e',
+            colorInputBackground:'#252a41',
+            colorInputText: '#fff'
+          }
+        }
+      }
+      >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${inter.className} bg-dark-2`}
+      >
+        <Toaster/>
+        {children}
+        
+      </body>
+      </ClerkProvider>
+      
+    </html>
+  );
+}
